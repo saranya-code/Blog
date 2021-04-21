6a99ac60afce38c192df976b960479bbeb916d8e
saranya-code <saranyadayalan28@gmail.com>
2021-04-21 12:52:06 -0500

    Attempted fix.

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ let connection;
 
 // create connection to our db
 if( process.env.JAWSDB_URL ){
-  connection  = new Sequelize(process.env.JAWSDB_URL)
+  connection = sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
   connection = new Sequelize(
     process.env.DB_NAME, 
-    process.env.DB_USER, 
+    process.env.DB_USER,
     process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
